5b66372a73b9a35d9d9baf9c0f0c908cae2a71cd
JohannesKle1ne <jojo.kleine@gmx.de>
2022-10-22 14:22:47 +0200

    added ip call counter

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 var express = require("express");
 var app = express();
 
+let lastIp;
+let counter = 0;
+
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  const ip = req.socket.remoteAddress;
+  if (ip === lastIp) counter++;
+  res.send(`Hello World! called ${counter} times from this ip: ${ip}`);
+  lastIp = ip;
 });
 
 app.get("/s", function (req, res) {
   const ip = req.socket.remoteAddress;
   res.send(`IP: ${ip}`);
+});
+
+app.get("/data", function (req, res) {
+  const ip = req.socket.remoteAddress;
+  if (ip === lastIp) {
+    res.sendStatus(404);
+  } else {
+    res.send(`Access granted`);
+  }
 });
 
 var server = app.listen(process.env.PORT || 8080, function () {
