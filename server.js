7c5647f52bc62aead6caef34f057ac815a1b05ad
JohannesKle1ne <jojo.kleine@gmx.de>
2022-10-22 13:20:37 +0200

    addedexpress files

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.get("/s", function (req, res) {
+  const ip = req.socket.remoteAddress;
+  res.send(`IP: ${ip}`);
+});
+
+var server = app.listen(8081, function () {
+  var port = server.address().port;
+  console.log(`Server running on Port ${port}`);
+});
